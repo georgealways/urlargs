5955cfd432982cfd5cd76cda5c1dd0ba81a2f174
George Michael Brower <george.brower@gmail.com>
2025-07-07 17:19:18 -0400

    describe

--- urlargs.ts
+++ urlargs.ts
@@ -64,7 +64,7 @@ export class UrlArgs<T extends Record<string, any>> {
 	 * ```
 	 */
 	public describe( descriptions: Partial<Record<keyof T, string>> = {} ): void {
-		const keys = Object.keys( this.defaults );
+		const keys = Object.keys( descriptions );
 		const rows: string[][] = [];
 		const styles: string[][] = [];
 		for ( const key of keys ) {
