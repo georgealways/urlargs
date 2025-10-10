f69d698538150d26545942da83b21ff4fe54d362
George Michael Brower <george.brower@gmail.com>
2025-10-10 12:28:55 -0400

    fix types

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,8 @@ import type { AllowedArg, ArrayArg, NullishArg } from './special.js';
 
 export type ResolveSpecial<T> = {
 	[K in keyof T]:
-	T[K] extends NullishArg<infer U, infer N>
-		? ( N extends undefined ? U | undefined : U | null )
+	T[K] extends NullishArg<infer U, any> & { defaultValue: infer D }
+		? ( undefined extends D ? U | undefined : U | null )
 		: T[K] extends ArrayArg<infer A> ? A[]
 			: T[K] extends AllowedArg<infer L> ? L
 				: T[K] extends boolean ? boolean : T[K]
