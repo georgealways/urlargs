c3b16f79f468bf70c778b7ed5502ef11947315bd
George Michael Brower <george.brower@gmail.com>
2025-10-11 19:23:21 -0400

    slightly more readable type resolver

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,25 @@
 import type { AllowedArg, ArrayArg, JsonArg, NullishArg } from './special.js';
 
+type ResolveNullishArg<T> =
+	T extends NullishArg<infer U, any> & { defaultValue: infer D }
+		? U | ( undefined extends D ? undefined : null )
+		: never;
+
+type ResolveAllowedArg<T> =
+	T extends AllowedArg<any, infer A>
+		? ( A extends readonly ( infer L )[] ? L : never )
+		: never;
+
+type ResolveArgType<T> =
+	T extends NullishArg<any, any> ? ResolveNullishArg<T>
+		: T extends ArrayArg<infer A> ? A[]
+			: T extends AllowedArg<any, any> ? ResolveAllowedArg<T>
+				: T extends JsonArg<infer J> ? J
+					: T extends boolean ? boolean
+						: T;
+
 export type ResolveSpecial<T> = {
-	[K in keyof T]:
-	T[K] extends NullishArg<infer U, any> & { defaultValue: infer D }
-		? ( undefined extends D ? U | undefined : U | null )
-		: T[K] extends ArrayArg<infer A> ? A[]
-			: T[K] extends AllowedArg<any, infer A>
-				? ( A extends readonly ( infer L )[] ? L : never )
-				: T[K] extends JsonArg<infer J> ? J
-					: T[K] extends boolean ? boolean : T[K]
+	[K in keyof T]: ResolveArgType<T[K]>
 };
 
 export type AllowedPrimitives = string | number | boolean;
